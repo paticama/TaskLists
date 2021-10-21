632f3a69211ee83dd787c6bad79ba23b851d2e2c
0w4n <jorgelevia@gmail.com>
2021-10-21 17:19:50 +0200

    Se agrega el proyecto

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const port = 5050;
+const app = express();
+
+//routes
+app.get('/', (req,res) => res.sendFile(path.resolve('public/index.html')));
+app.use(express.static(path.join(__dirname, '/public')));
+//app.use('/upload',Upfile);
+//app.use('/download',DownFile);
+//app.use('/dir',DirRouter);
+
+app.listen(port, () => console.log('Server is runing in the port', port));
---
+++ public/index.js
@@ -0,0 +1,7 @@
+const btn_agrear = document.getElementById('.colgar-btn');
+//const btn_lapiz = document.getElementById('');
+//const btn_eliminar = document;
+                    /*Lista de tareas*/
+const titulo_tarea = document.getElementById('.titular');
+const materia = document.getElementById('materia');
+const ud = document.getElementById('.unidades');
